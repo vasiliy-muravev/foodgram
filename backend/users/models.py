639d2c833b5abe3a93a8190fe65f3ad86739b6dd
Vasily Muraviev <muravievvv@develop.mos.ru>
2024-10-19 14:58:24 +0300

    flake8

--- backend/users/models.py
+++ backend/users/models.py
@@ -1,6 +1,6 @@
 from django.db import models
-from django.contrib.auth.models import AbstractUser
 from django.db.models import CharField
+from django.contrib.auth.models import AbstractUser
 
 from users.constants import MAX_LENGTH
 
